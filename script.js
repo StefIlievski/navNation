3e8e0d62138b4ab1c6cdb998d92271cdf682ea36
Stefan Ilievski <stefan_ilievski994@hotmail.com>
2023-06-10 23:57:08 +0200

    Improving CSS animations

--- script.js
+++ script.js
@@ -15,10 +15,41 @@ function toggleNav() {
     // Animate In - Overlay
     overlay.classList.add("overlay-slide-right");
     overlay.classList.remove("overlay-slide-left");
+    // Animate In - Nav Items
+    nav1.classList.remove("slide-out-1");
+    nav1.classList.add("slide-in-1");
+
+    nav2.classList.remove("slide-out-2");
+    nav2.classList.add("slide-in-2");
+
+    nav3.classList.remove("slide-out-3");
+    nav3.classList.add("slide-in-3");
+
+    nav4.classList.remove("slide-out-4");
+    nav4.classList.add("slide-in-4");
+
+    nav5.classList.remove("slide-out-5");
+    nav5.classList.add("slide-in-5");
   } else {
     // Animate Out - Overlay
     overlay.classList.add("overlay-slide-left");
     overlay.classList.remove("overlay-slide-right");
+
+    // Animate Out - Nav Items
+    nav1.classList.remove("slide-in-1");
+    nav1.classList.add("slide-out-1");
+
+    nav2.classList.remove("slide-in-2");
+    nav2.classList.add("slide-out-2");
+
+    nav3.classList.remove("slide-in-3");
+    nav3.classList.add("slide-out-3");
+
+    nav4.classList.remove("slide-in-4");
+    nav4.classList.add("slide-out-4");
+
+    nav5.classList.remove("slide-in-5");
+    nav5.classList.add("slide-out-5");
   }
 }
 
